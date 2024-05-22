f5e044a1c8fcf6c1768db774c7728ba6a39a1e27
allatifatul <allatifatulmuharomiah@students.unnes.ac.id>
2024-05-22 07:29:44 +0700

    Initial commit

---
+++ app/src/main/java/android/example/food/Splashscreen.java
@@ -0,0 +1,34 @@
+package android.example.food;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Splashscreen extends AppCompatActivity {
+
+    private int waktu_loading=4000;
+    //4000=4 detik
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+//setelah loading maka akan langsung berpindah ke Main activity
+                Intent home = new Intent(Splashscreen.this,
+                        Welcome.class);
+                startActivity(home);
+                finish();
+            }
+        }, waktu_loading);
+    }
+
+}
