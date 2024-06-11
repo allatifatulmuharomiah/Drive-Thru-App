13faf3c5b9da3a49c75185c745ff956b86924de5
allatifatul <allatifatulmuharomiah@students.unnes.ac.id>
2024-06-12 01:57:07 +0700

    dashboard blm fix

--- app/src/main/java/android/example/food/MainActivity.java
+++ app/src/main/java/android/example/food/MainActivity.java
@@ -1,7 +1,10 @@
 package android.example.food;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,13 +17,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+
+    public void drive(View view) {
+        startActivity(new Intent(MainActivity.this,Menu.class));
     }
 
 
